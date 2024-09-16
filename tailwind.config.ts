750846133b2992fff86ea06438574df2c95d09e7
Brad <klausmickelson52@gmail.com>
2024-09-16 16:36:55 +0100

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +9,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        accent: "#00B2A1",
+      },
+      fontSize: {
+        s13: ["0.81rem", "0.99rem"],
+        s20: ["1.25rem", "1.88rem"],
+        s26: ["1.625rem", "1.98rem"],
+        s34: ["2.13rem", "1.69rem"],
+        s46: ["2.88rem", "3.51rem"],
+        s54: ["3.38rem", "3.38rem"],
+      },
+      spacing: {
+        "18": "1.125rem",
+      },
+      dropShadow: {
+        card: "0px -1px 20px #0000000D",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
